413981d7d608321833e44394124d24f62a323744
Nieves Alonso Gilsanz <nievalon@ucm.es>
2026-03-05 12:01:01 +0100

    pasado movimiento jugador

--- Assets/Scripts/Comportamientos/ControlJugador.cs
+++ Assets/Scripts/Comportamientos/ControlJugador.cs
@@ -1,8 +1,8 @@
-/*    
+Ôªø/*    
    Copyright (C) 2020-2023 Federico Peinado
    http://www.federicopeinado.com
    Este fichero forma parte del material de la asignatura Inteligencia Artificial para Videojuegos.
-   Esta asignatura se imparte en la Facultad de Inform·tica de la Universidad Complutense de Madrid (EspaÒa).
+   Esta asignatura se imparte en la Facultad de Inform√°tica de la Universidad Complutense de Madrid (Espa√±a).
    Autor: Federico Peinado 
    Contacto: email@federicopeinado.com
 */
@@ -17,7 +17,7 @@ namespace UCM.IAV.Movimiento
     public class ControlJugador: ComportamientoAgente
     {
         /// <summary>
-        /// Obtiene la direcciÛn
+        /// Obtiene la direcci√≥n
         /// </summary>
         /// <returns></returns>
         /// 
@@ -25,21 +25,97 @@ namespace UCM.IAV.Movimiento
         //float tiempoGiroSuave = 0.1f;
         //float velocidadGiroSuave;
 
+        [SerializeField]
+        float minimuRadius = 3.0f; // radio alrededor del jugador en el que no moverse
+
+        private float velocidadMaxNormal;
+        private float velocidadMaxRapida;
+        private float aceleracionNormal;
+        private float aceleracionRapida;
+
+        private bool able = true;
+        private bool sprinting = false;
+
         public override Direccion GetDireccion()
         {
+            Direccion direccion = new Direccion();
+
+            Debug.Log("0");
+
+            if (!able)
+            {
+                Debug.Log("1");
+                return direccion;
+            }
+                
+            // Direccion actual
+            // Control por teclado
+            direccion.lineal.x = Input.GetAxis("Horizontal");
+            direccion.lineal.z = Input.GetAxis("Vertical");
+
+            // Control por raton
+            RaycastHit hit;
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            int layerMask = 1 << 8;
+
+            // Si apuntamos a un sitio valido
+            if (Physics.Raycast(ray, out hit, 100, layerMask))
+            { // Cogemos la direccion y nos congelamos en altura
+
+                Debug.Log("eing");
+                direccion.lineal = hit.point - transform.position;
+                direccion.lineal.y = 0;
+            }
+
+            // Si la colision, aunque valida esta en un radio cercano al jugador
+            if (direccion.lineal.magnitude < minimuRadius)
+            {
+                Debug.Log("2");
+                return new Direccion();
+            }
+
+            // Comprobamos si estamos corriendo
+            sprinting = Input.GetKey(KeyCode.Mouse0);
+
+            if (sprinting)
+            {
+                agente.aceleracionMax = aceleracionRapida;
+                agente.velocidadMax = velocidadMaxRapida;
+                Debug.Log("3");
+            }
+            else
+            {
+                agente.aceleracionMax = aceleracionNormal;
+                agente.velocidadMax = velocidadMaxNormal;
+                Debug.Log("4");
+            }
+
+            // Resto de calculo de movimiento
+            direccion.lineal.Normalize();
+            direccion.lineal *= agente.aceleracionMax;
+
+            Debug.Log("5");
+
+            return direccion;
+        }
+
+        // MOVIMIENTO CON WASD
+        /*
+        public override Direccion GetDireccion()
+        {
+
             Direccion direccion = new Direccion();
             
             //Direccion actual
             direccion.lineal.x = Input.GetAxis("Horizontal");
             direccion.lineal.z = Input.GetAxis("Vertical");
 
-            //Resto de c·lculo de movimiento
+            //Resto de calculo de movimiento
             direccion.lineal.Normalize();
             direccion.lineal *= agente.aceleracionMax;
 
-            // PodrÌamos meter una rotaciÛn autom·tica en la direcciÛn del movimiento, si quisiÈramos
-
             return direccion;
         }
+        */
     }
 }
